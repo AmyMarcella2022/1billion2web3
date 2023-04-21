4af8b80ee857c6d30a8d84a2972f224cd329539d
Neto <ukpongneto@wellahealth.com>
2023-04-21 10:11:23 +0100

    overflow-y added to card

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -126,7 +126,7 @@ const Game = () => {
   return (
     <div className='w-full max-w-md p-6'>
       <h2 className={`${styles.heading2} text-center`}>Web3 Quest Journey</h2>
-      <div className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+      <div className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 overflow-y-scroll'>
         <div className='flex flex-row'>
           <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
             {questionNumber + 1}
@@ -136,7 +136,7 @@ const Game = () => {
               {questions[questionNumber]?.question}
             </h4>
             {questions[questionNumber]?.options.map((option, index) => (
-              <label key={index} className='inline-flex items-center overflow-y-scroll'>
+              <label key={index} className='inline-flex items-center'>
                 <input
                   type='radio'
                   className='form-radio h-4 w-4'
