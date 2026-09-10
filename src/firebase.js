779f925d73fcf4c025ed38c7a0ed65bab5af4976
AmyMarcella2022 <100494749+AmyMarcella2022@users.noreply.github.com>
2026-09-10 10:15:12 +0100

    Fix formatting of firebaseConfig object

--- src/firebase.js
+++ src/firebase.js
@@ -33,13 +33,13 @@ const firebaseConfig = {
 };
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBi9PF8pQlBFz8_KwnQckDboS62HNgiaLA",
-    authDomain: "billion2web3.firebaseapp.com",
-    projectId: "billion2web3",
-    storageBucket: "billion2web3.firebasestorage.app",
-    messagingSenderId: "79163528871",
-    appId: "1:79163528871:web:150c9ed7e560253b4f2d82",
-    measurementId: "G-TYKXCFWXB0"
+  apiKey: "AIzaSyBi9PF8pQlBFz8_KwnQckDboS62HNgiaLA",
+  authDomain: "billion2web3.firebaseapp.com",
+  projectId: "billion2web3",
+  storageBucket: "billion2web3.firebasestorage.app",
+  messagingSenderId: "79163528871",
+  appId: "1:79163528871:web:150c9ed7e560253b4f2d82",
+  measurementId: "G-TYKXCFWXB0"
 };
 
 const firebaseConfig = import.meta.env.PROD ? firebaseProdConfig : firebaseDevConfig;
