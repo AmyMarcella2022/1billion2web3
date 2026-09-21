742143f8ba8e6886e22e279105aec46da4b58003
AmyMarcella2022 <100494749+AmyMarcella2022@users.noreply.github.com>
2026-09-21 20:16:14 +0100

    Add files via upload

--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -87,6 +87,7 @@ export const dashboardAccordionList = [
     content: {
       metaverse:
         'https://coral-fantastic-worm-816.mypinata.cloud/ipfs/bafybeihb3vo3evqpbnbxhyyuei77c6si5bpw3dhi2i5pr45lmhxs5zjci4',
+      videoTitle: 'WEB3 Mindset Reset',
       path: '/module/1',
     },
   },
@@ -96,6 +97,7 @@ export const dashboardAccordionList = [
     content: {
       metaverse:
         'https://coral-fantastic-worm-816.mypinata.cloud/ipfs/bafybeif7agoiqsifrc5cgdp7sf5eyeaf4vj5ofb7jpciu3fwx7awqkp6by',
+      videoTitle: 'WEB3 Blockchain Technology',
       path: '/module/2',
     },
   },
@@ -105,6 +107,7 @@ export const dashboardAccordionList = [
     content: {
       metaverse:
         'https://coral-fantastic-worm-816.mypinata.cloud/ipfs/bafybeih2zsot7oxw3xo53ea2ikgmjkrogdcgvqodc2wbihclwmzxy5phyy',
+      videoTitle: 'WEB3 Privacy',
       path: '/module/3',
     },
   },
@@ -114,6 +117,7 @@ export const dashboardAccordionList = [
     content: {
       metaverse:
         'https://coral-fantastic-worm-816.mypinata.cloud/ipfs/bafybeie2tapwlehknlfk44ix632bnkjq6f6f43jcrryvpmzvfj32cmcd5y',
+      videoTitle: 'WEB3 Cryptocurrency',
       path: '/module/4',
     },
   },
@@ -123,6 +127,7 @@ export const dashboardAccordionList = [
     content: {
       metaverse:
         'https://coral-fantastic-worm-816.mypinata.cloud/ipfs/bafybeihc64v2r3nquhlesrlmao3vzttvufhiqjz6yb5ihenmu745lk3anm',
+      videoTitle: 'WEB3 DAO',
       path: '/module/5',
     },
   },
@@ -132,6 +137,7 @@ export const dashboardAccordionList = [
     content: {
       metaverse:
         'https://coral-fantastic-worm-816.mypinata.cloud/ipfs/bafybeibd4c3ljpelpqal3a5kzqo6d3ghnsuz6sislsdvbjogkhqmjlieae',
+      videoTitle: 'WEB3 NFTs',
       path: '/module/6',
     },
   },
@@ -141,6 +147,7 @@ export const dashboardAccordionList = [
     content: {
       metaverse:
         'https://coral-fantastic-worm-816.mypinata.cloud/ipfs/bafybeiaycphwurmy7a4ro5rzljl5umbjwg7qermi5m4kwjrjk4ef5tksia',
+      videoTitle: 'WEB3 Smart Contract',
       path: '/module/7',
     },
   },
@@ -150,6 +157,7 @@ export const dashboardAccordionList = [
     content: {
       metaverse:
         'https://coral-fantastic-worm-816.mypinata.cloud/ipfs/bafybeib63kftgtjtouar2lo7kjwx2cliopalsgubj2pgfb7oqlr4lpqhby',
+      videoTitle: 'What Is A Metaverse',
       path: '/module/8',
     },
   },
@@ -159,6 +167,7 @@ export const dashboardAccordionList = [
     content: {
       metaverse:
         'https://coral-fantastic-worm-816.mypinata.cloud/ipfs/bafybeiae7gjkyp4jx6t5ktdzrgrefwow2s3evdteeedcgqxwz5h3zbtphe',
+      videoTitle: 'WEB3 Gaming',
       path: '/module/9',
     },
   },
